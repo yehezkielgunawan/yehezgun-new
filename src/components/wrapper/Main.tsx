adbe6dd4e5060a8e2bf2f395c5678dd10b9f0fdc
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-12-12 20:06:51 +0700

    style(Main.tsx): :lipstick: add transition animation slideFade

--- src/components/wrapper/Main.tsx
+++ src/components/wrapper/Main.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "@chakra-ui/layout";
+import { SlideFade } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
 
 type MainProps = {
@@ -7,22 +8,24 @@ type MainProps = {
 
 const Main = ({ children }: MainProps) => {
   return (
-    <Stack
-      variants={{
-        before: { opacity: 0, y: 25, transition: { type: "spring" } },
-        after: { opacity: 1, y: 0, transition: { type: "spring" } },
-      }}
-      initial="before"
-      animate="after"
-      width="100%"
-      maxWidth="48rem"
-      pt="4rem"
-      px={[3, 1]}
-      mt={8}
-      spacing={4}
-    >
-      {children}
-    </Stack>
+    <SlideFade in offsetY="20px">
+      <Stack
+        variants={{
+          before: { opacity: 0, y: 25, transition: { type: "spring" } },
+          after: { opacity: 1, y: 0, transition: { type: "spring" } },
+        }}
+        initial="before"
+        animate="after"
+        width="100%"
+        maxWidth="48rem"
+        pt="4rem"
+        px={[3, 1]}
+        mt={8}
+        spacing={4}
+      >
+        {children}
+      </Stack>
+    </SlideFade>
   );
 };
 
