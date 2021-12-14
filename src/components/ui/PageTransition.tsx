588053c80f91bd65abc347946d1735d1a15f303d
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-12-14 11:52:16 +0700

    style(pageTransition.tsx): :lipstick: add offset for pageTransition

--- src/components/ui/PageTransition.tsx
+++ src/components/ui/PageTransition.tsx
@@ -2,7 +2,7 @@ import { SlideFade, SlideFadeProps } from "@chakra-ui/react";
 import React from "react";
 
 const PageTransition = (props: SlideFadeProps) => {
-  return <SlideFade in {...props} />;
+  return <SlideFade in offsetY="20px" {...props} />;
 };
 
 export default PageTransition;
