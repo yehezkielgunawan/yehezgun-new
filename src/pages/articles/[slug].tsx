40671911d2841a1feb44070fff595e35380b6e7f
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-01-01 20:56:51 +0700

    perf(articles): :art: change ISR to SSG for article list and post

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -41,7 +41,6 @@ export async function getStaticProps({
     props: {
       postData,
     },
-    revalidate: 30,
   };
 }
 
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -23,7 +23,6 @@ export async function getStaticProps() {
     props: {
       articleList,
     },
-    revalidate: 30,
   };
 }
 
