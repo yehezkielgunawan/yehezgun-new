12c19ecee9bb2d6dae7301df19e0bcb292298850
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-01-24 21:06:25 +0700

    refactor(yehezOgImage): :recycle: add encodeuri for og image

--- src/functions/helpers/yehezOgImage.ts
+++ src/functions/helpers/yehezOgImage.ts
@@ -1,6 +1,10 @@
 export const yehezOgImage = (text: string, isArticle: boolean) => {
   if (isArticle) {
-    return `https://yehez-og-image.yehezgun.com/${text}.png?theme=dark&md=1&fontSize=75px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=300&heights=300`;
+    return `https://yehez-og-image.yehezgun.com/${encodeURI(
+      text
+    )}.png?theme=dark&md=1&fontSize=75px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=300&heights=300`;
   }
-  return `https://yehez-og-image.yehezgun.com/**${text}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1636202181%2Fpeep_amkhuu.svg&widths=250&heights=250`;
+  return `https://yehez-og-image.yehezgun.com/**${encodeURI(
+    text
+  )}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1636202181%2Fpeep_amkhuu.svg&widths=250&heights=250`;
 };
