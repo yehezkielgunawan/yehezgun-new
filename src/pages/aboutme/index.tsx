6e86f41e7e98baccb1d9650b8ae01c6f6fc9dc23
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-11 19:44:43 +0700

    fix(aboutme): :bug: fix isArticle flag into false

--- src/pages/aboutme/index.tsx
+++ src/pages/aboutme/index.tsx
@@ -47,7 +47,7 @@ function AboutMe({ experienceList }: { experienceList: Experiences }) {
           pageTitle="About Me"
           pageDesc="A little description about me."
           route="aboutme"
-          isArticle={true}
+          isArticle={false}
         />
         <Flex justifyContent="space-between" gridGap={[4, 8]} align="center">
           <Stack spacing={[3, 4]}>
