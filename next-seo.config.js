48875facef73eb8f11059a34488d572b07d84aad
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-14 21:37:34 +0700

    chore(next-seo): :wrench: remove fixed width and height for og image

--- next-seo.config.js
+++ next-seo.config.js
@@ -14,8 +14,6 @@ const defaultSEOConfig = {
       {
         url: "https://og.yehezgun.com/api/base?description=An%20online%20portfolio%20and%20article%20site%20by%20Yehezkiel%20Gunawan.%20Showcase%20about%20my%20projects%20and%20some%20thoughts%20about%20my%20experience%20in%20web%20development.&siteName=yehezgun.com&templateTitle=Yehezkiel%20Gunawan&theme=dark",
         alt: "yehezgun.com og-image",
-        width: 800,
-        height: 600,
       },
     ],
     site_name: "yehezgun",
